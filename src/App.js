671faa9014be5278c3196b2de363f09c1e38d6aa
Selfmade20 <tebogo1686@gmail.com>
2025-02-19 20:22:59 +0200

    Creating the home page and styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Header from './Components/Header/Header';
+import Home from './Components/Home/Home';
 
 function App() {
   return (
     <div>
       <Header />
+      <main className='main'>
+        <Home />
+      </main>
     </div>
   );
 }
