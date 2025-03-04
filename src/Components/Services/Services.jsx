bbd94a91966d66724fa6fca4876b954a94612d8a
Selfmade20 <tebogo1686@gmail.com>
2025-03-04 19:55:50 +0200

    Finalising the state of the toggle tabs

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -62,14 +62,14 @@ const Services = () => {
                 <h3 className="services__title">Back End <br/> Developer</h3>
             </div>
 
-            <span className="services__button">View More 
+            <span className="services__button" onClick={() => toggleTab(2)}>View More 
                 <i className="uil uil-arrow-right services__button-icon"></i>
             </span>
 
-            <div className="">
+            <div className={toggleState === 2 ? "services__modal active-modal" : "services__modal"}>
                 <div className="services__modal-content">
-                    <i className="uil uil-times services_modal-close"></i>
-                    <h3 className="services__modal-title">Back-End Developer</h3>
+                    <i onClick={() => toggleTab(0)} className="uil uil-times services__modal-close"></i>
+                    <h3 className="services__modal-title">Back End Developer</h3>
                     <p className="services__modal-description">Services with more than 3 years of professional experience, providing quality work to clients and companies</p>
 
                     <ul className="services__modal-services grid">
@@ -104,35 +104,35 @@ const Services = () => {
                 <h3 className="services__title">Application <br/> Developer</h3>
             </div>
 
-            <span className="services__button" onClick={() => toggleTab(1)}>View More 
+            <span className="services__button" onClick={() => toggleTab(3)}>View More 
                 <i className="uil uil-arrow-right services__button-icon"></i>
             </span>
 
-            <div className="services__modal">
+            <div className={toggleState === 3 ? "services__modal active-modal" : "services__modal"}>
                 <div className="services__modal-content">
-                    <i className="uil uil-times services_modal-close"></i>
-                    <h3 className="services__modal-title">Full Stack Developer</h3>
+                    <i onClick={() => toggleTab(0)} className="uil uil-times services__modal-close"></i>
+                    <h3 className="services__modal-title">Mobile Application Developer</h3>
                     <p className="services__modal-description">Services with more than 3 years of professional experience, providing quality work to clients and companies</p>
 
                     <ul className="services__modal-services grid">
                         <li className="services__modal-service">
                             <i className="uil uil-check-circle services__modal-icon"></i>
                             <p className="services__modal-info">
-                                I develop user interfaces.
+                                Yet To Come......
                             </p>
                         </li>
 
                         <li className="services__modal-service">
                             <i className="uil uil-check-circle services__modal-icon"></i>
                             <p className="services__modal-info">
-                                Webpage Development.
+                                Yet To Come.......
                             </p>
                         </li>
 
                         <li className="services__modal-service">
                             <i className="uil uil-check-circle services__modal-icon"></i>
                             <p className="services__modal-info">
-                                IT customer support.
+                                Yet To Come......
                             </p>
                         </li>
                     </ul>
